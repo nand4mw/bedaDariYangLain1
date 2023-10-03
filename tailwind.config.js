4704c0289951d282cb1b2287e8d0886374136eb0
4nndmw <anandamaulanaw1850@gmail.com>
2023-10-03 12:58:25 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    container: {
+      colors: {
+        blue: "#8591ec",
+      },
+    },
+
+    extend: {},
+  },
+  plugins: [],
+};
